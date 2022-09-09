fc1b4bbd020f65f4cd04195e2868bad3ab29cf3f
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-09 11:31:49 +0530

    Initialized frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,10 @@
+//imports
 import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app: Application = express();
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
